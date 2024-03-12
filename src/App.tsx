b96cee2b076b136f91d665c80995692d49702c2f
skornya <kornyakovS@gmail.com>
2024-03-12 13:00:05 +0300

    fix grid, refactor, add title

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
-import { Card, Col, ConfigProvider, Grid, Layout, Row, theme } from 'antd';
-import { FunctionComponent, useEffect, useState } from 'react';
+import { FunctionComponent, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import { Card, Col, ConfigProvider, Row, Skeleton, theme } from 'antd';
+import Title from 'antd/es/typography/Title';
+
 import Goods from './components/Goods';
 import Total from './components/Total';
 
-import { fetchGoods } from '../utils/fetchGoods';
-// import './App.css';
+import { fetchGoods } from './utils/fetchGoods';
+import { RootState, useAppDispatch } from './store';
+import { setError, setLoading } from './model/statusReducer';
+import { addGood } from './model/cartReducer';
 
 const App: FunctionComponent = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>('');
+  const dispatch = useAppDispatch();
+  const status = useSelector((state: RootState) => state.status);
+  const { loading, error } = status;
 
   useEffect(() => {
     const getGoods = async () => {
       try {
+        dispatch(setLoading(true));
         const data = await fetchGoods();
-        
+        data.forEach((good) => {
+          dispatch(addGood(good));
+        });
+        dispatch(setLoading(false));
       } catch (error) {
         if (error instanceof Error) {
-          setError(error.message);
+          dispatch(setError(error.message));
         }
       }
     };
-  }, []);
+
+    getGoods();
+  }, [dispatch]);
 
   return (
     <ConfigProvider
@@ -29,14 +42,19 @@ const App: FunctionComponent = () => {
         algorithm: theme.darkAlgorithm,
       }}
     >
-      <Row>
-        <Col flex={3}>
-          <Goods />
-        </Col>
-        <Col flex={1}>
-          <Total />
-        </Col>
-      </Row>
+      <Title level={3}>Корзина</Title>
+      {!error ? (
+        <Row gutter={20}>
+          <Col span={18} style={{ height: '100vh' }}>
+            {!loading ? <Goods /> : <Skeleton active />}
+          </Col>
+          <Col span={6}>
+            <Total />
+          </Col>
+        </Row>
+      ) : (
+        <Card>{error}</Card>
+      )}
     </ConfigProvider>
   );
 };
