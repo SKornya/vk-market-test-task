f5594e120219afd82815177b093ec70ba01eff4a
skornya <kornyakovS@gmail.com>
2024-03-12 12:59:23 +0300

    changet type import

---
+++ src/utils/fetchGoods.ts
@@ -0,0 +1,16 @@
+import { Good } from "../model/cartReducer";
+
+const DATA_API_URL = 'https://fakestoreapi.com/products?limit=20';
+
+const fetchGoods = async (): Promise<Good[]> => {
+  const response = await fetch(DATA_API_URL);
+
+  if (!response.ok) {
+    throw Error(`response status is ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+export { fetchGoods };
