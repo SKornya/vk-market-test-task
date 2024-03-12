f3faf20c7c22adf977a063b3ad59eba5075e0ff3
skornya <kornyakovS@gmail.com>
2024-03-12 12:57:42 +0300

    right col

--- src/components/Total.tsx
+++ src/components/Total.tsx
@@ -1,7 +1,22 @@
 import { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+
+import { Button } from 'antd';
+import Text from 'antd/es/typography/Text';
+
+import { RootState } from '../store';
 
 const Total: FunctionComponent = () => {
-  return <></>;
+  const cart = useSelector((state: RootState) => state.cart);
+  const { totalPrice } = cart;
+
+  return (
+    <Button size="large" type="primary" style={{ width: '100%' }}>
+      <Text strong>
+        Итого: {totalPrice} {'\u0024'}
+      </Text>
+    </Button>
+  );
 };
 
 export default Total;
