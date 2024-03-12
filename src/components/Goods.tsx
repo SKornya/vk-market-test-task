7d80ad7211f50ae66a86a154b120872a83f94bab
skornya <kornyakovS@gmail.com>
2024-03-12 12:59:37 +0300

    render list of goods

--- src/components/Goods.tsx
+++ src/components/Goods.tsx
@@ -1,7 +1,23 @@
 import { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
+
+import GoodCard from './GoodCard';
+
+import { RootState } from '../store';
 
 const Goods: FunctionComponent = () => {
-  return <></>;
+  const cart = useSelector((state: RootState) => state.cart);
+  const { goods } = cart;
+
+  return (
+    <>
+      {goods.map(({ item, quantity }) => {
+        return (
+          <GoodCard item={item} quantity={quantity} />
+        );
+      })}
+    </>
+  );
 };
 
 export default Goods;
